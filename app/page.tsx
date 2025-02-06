d169d7236ef985b86e0bf6baf49078ddee615018
Sasank-V <sasanklearner@gmail.com>
2025-02-07 02:09:24 +0530

    Google OAuth Configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,24 @@
-const page = () => {
-  return <div>page</div>;
+"use client";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [name, setName] = useState("");
+  const { data: session } = useSession();
+  const handleSignIn = async () => {
+    signIn("google");
+  };
+  useEffect(() => {
+    if (session?.user?.name) {
+      setName(session.user.name);
+    }
+  }, [session]);
+
+  return (
+    <div>
+      <p>{name}</p>
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
+  );
 };
-export default page;
+export default Home;
