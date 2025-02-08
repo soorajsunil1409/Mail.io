436e38307d5953530af11705017f67a8b70535a8
Sasank-V <sasanklearner@gmail.com>
2025-02-08 15:25:27 +0530

    Works

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { IUser, User } from "@/models/User";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { connect_DB } from "../utils/DB";
+import { connect_DB } from "@/utils/DB";
 import { google } from "googleapis";
 import { OAuth2Client } from "google-auth-library";
 import { defaultCategories } from "@/lib/constants";
