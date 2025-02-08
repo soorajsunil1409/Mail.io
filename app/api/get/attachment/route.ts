15f48039eab296a29b7d426554b31d686424cb5a
Sasank-V <sasanklearner@gmail.com>
2025-02-08 15:17:33 +0530

    Fast as fuck boi

--- app/api/get/attachment/route.ts
+++ app/api/get/attachment/route.ts
@@ -1,10 +1,27 @@
-export async function GET(request: Request) {
+import { google } from "googleapis";
+import { oauth2Client } from "@/lib/auth";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
   try {
-  } catch (error) {
-    console.log("Error while fetching Attachment: ", error);
+    const searchParams = request.nextUrl.searchParams;
+    const message_id = searchParams.get("message_id");
+    const attachment_id = searchParams.get("attachment_id");
+
+    const gmail = google.gmail({ version: "v1", auth: oauth2Client });
+    const attachmentRes = gmail.users.messages.attachments.get({
+      userId: "me",
+      messageId: message_id,
+      id: attachment_id,
+    });
+    console.log(attachmentRes);
     return Response.json({
-      success: false,
-      error: "Error While getting Attachment",
+      attachmentRes,
+    });
+  } catch (error) {
+    console.log("Error while Fetching User Categories");
+    return Response.json({
+      error,
     });
   }
 }
